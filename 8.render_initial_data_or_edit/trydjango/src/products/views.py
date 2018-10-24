12dd23b9754f73a6199fe42c84945da6d15890c7
maainul <mainul080@hotmail.com>
2018-10-24 02:07:27 -0400

    edit form

---
+++ 8.render_initial_data_or_edit/trydjango/src/products/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from .models import Product
+from .forms import ProductForm,RawProductForm
+
+def product_create_view(request):
+	form = ProductForm(request.POST or None)
+	
+	if form.is_valid():
+		form.save()
+
+	form = ProductForm()
+
+	context={"form": form}
+
+	return render(request,"products/product_create.html",context)
+
+# def product_create_view(request):
+# 	my_form = RawProductForm()
+# 	if request.method == "POST":
+# 		my_form = RawProductForm(request.POST)
+# 		if my_form.is_valid():
+# 			print(my_form.cleaned_data)
+# 			Product.objects.create(**my_form.cleaned_data)
+# 		else:
+# 			print(my_form.errors)
+# 	context={"form": my_form}
+# 	return render(request,"products/product_create.html",context)
+
+
+def product_detail_view(request):
+	obj = Product.objects.get(id=1)
+	#context={
+	#	'title':obj.title,
+	#	'description':obj.description,
+	#	'price':obj.price,
+	#	'summary':obj.summary
+	#}
+	context={
+		"object":obj
+	}
+
+	return render(request,"products/product_detail.html",context)
+
+
+def render_initial_data(request):
+	initial_data = {'title': "this is my title"}
+	obj = Product.objects.get(id=1)
+	form = ProductForm(request.POST or None,instance = obj)
+	if form.is_valid():
+		form.save()
+	context={'form':form}
+	return render(request,"products/product_edit.html",context)
---
+++ 8.render_initial_data_or_edit/trydjango/src/trydjango/urls.py
@@ -0,0 +1,32 @@
+"""trydjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from pages import views
+from django.contrib import admin
+from django.urls import path
+from pages.views import home_view,about_view,social_view,contact_view
+from products.views import product_detail_view,product_create_view,render_initial_data
+urlpatterns = [
+    path('admin/', admin.site.urls),
+	path('', views.home_view, name='home'),
+
+	path('about/',about_view),
+	path('contact/',contact_view),
+	path('social/',social_view),
+
+    path('product/',product_detail_view),
+    path('create/',product_create_view),
+    path('edit/',render_initial_data),
+]
